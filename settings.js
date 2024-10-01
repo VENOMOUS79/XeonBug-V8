7263fea86942c80c217afd77b78c1eeaa6105fc3
VENOMOUS79 <xxxtentacionmusuc@gmail.com>
2024-10-01 16:06:44 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "2349136934806"
+global.ownername = "Venom"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '2349136934806'  //creator number
+global.ownername = 'venom' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
